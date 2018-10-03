1ae17a3495db8e2a176b423f3dbfecc9afc167fa
Torxgewinde <Torxgewinde@users.noreply.github.com>
2018-10-03 16:28:42 +0200

    Example script only changes volume if out of sync

--- example/keep_volume_in_sync.sh
+++ example/keep_volume_in_sync.sh
@@ -10,7 +10,7 @@
 #
 ###############################################################################
 
-HOST="webcam.lan"
+HOST="forked-daapd.server.lan"
 OUTPUTS="Audio-1 Audio-2 Computer"
 
 function task() {
@@ -18,21 +18,34 @@ function task() {
 	VOL=$(mpc -h "$HOST" volume | sed -e "s/[^0-9]*\([0-9]*\).*/\1/g")
 
 	echo "Master volume is: $VOL percent"
-
+	
 	for i in $OUTPUTS; do
-		#keep these outputs active are reactivate if they were absent in between
-		mpc -h "$HOST" enable "$i"
-		python output.py "$i" "$VOL"
+		# ensure our outputs are enabled al the time
+		mpc -h "$HOST" enable "$i" > /dev/null
+
+		THIS_INFO=$(python output.py "$i")
+		THIS_VOL=$(echo "$THIS_INFO" | grep outputvolume | sed -e "s/[^0-9]*\([0-9]*\)/\1/g")
+		THIS_ENABLE=$(echo "$THIS_INFO" | grep outputenabled | sed -e "s/[^0-9]*\([0-9]*\)/\1/g")
+
+		if [ "$THIS_ENABLE" != "1" ]; then
+			echo "output not be enabled, seems to be offline. Skipping..."
+			continue
+		fi
+
+		if [ "$VOL" != "$THIS_VOL" ]; then
+			python output.py "$i" "$VOL"
+		fi
 	done
+
+	sleep 5
 }
 
 # run the function task more or less as a daemon (detached from the console etc.)
-(0<&- 2<&- 1<&- $(cd /tmp && while true; do task; sleep 1; done) &)
-exit
+# (0<&- 2<&- 1<&- $(cd /tmp && while true; do task; done) &)
+# exit
 
 ###############################################################################
 # run it in the current console, no detach
 while true; do
 	task
-	sleep 1
 done
